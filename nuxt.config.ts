82f3d667ee826ea277e25cdc65f1aa351fff7f8e
Maks Stankevich <maksstankevich81@gmail.com>
2024-11-09 18:05:51 +0100

    start project

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: "@import '@/assets/styles/main.scss';",
+				},
+			},
+		},
+	},
+	imports: {
+		dirs: ['types/constants'],
+		presets: [
+			{
+				from: 'vue',
+				imports: ['withDefaults', 'defineProps', 'defineEmits'],
+			},
+			{
+				from: 'date-fns',
+				imports: ['format'],
+			},
+			{
+				from: '@splinetool/runtime',
+				imports: ['Application'],
+			},
+		],
+	},
+	modules: ['nuxt-icons'],
+	plugins: [{ src: '~/plugins/aos.js', mode: 'client' }],
+
+	app: {
+		head: {
+			title: 'LeoParts - sklep motoryzacyjny',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{
+					hid: 'description',
+					name: 'description',
+					content: 'CONTENT CONTENT CONTENT',
+				},
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+});
